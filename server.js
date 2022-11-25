d28e01724911519cf418873ffed6dc8609bb6cae
Maureen Höfferle <m.hofferle@gmail.com>
2022-11-25 17:31:26 -0300

    commit con primero hbs

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const app = express()
+var hbs = require('hbs');
 const port = 3000
+
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 
 app.use(express.static('public'));
 
-//app.get('/', (req, res) => {
-//  res.send('Hello World!')
-// })
+app.get('/', (req, res) => {
+    res.render("index",{
+        titulo: "Mi página web"
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render("about",{
+        titulo: "Sobre Nosotros"
+    })
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
